a9210bdfe510290be37a51b336faa643124fec25
quan <quan5080@gmail.com>
2021-10-30 21:04:54 +0700

    fix

--- src/custom/format.js
+++ src/custom/format.js
@@ -1,7 +1,7 @@
 export const formatNumber = (num) => {
   if (!num) return 0;
   if (num < 1000) return num;
-  else if (num < 1000000) return `${num * 0.001}K`;
+  else if (num < 1000000) return `${(num * 0.001).toFixed(1)}K`;
   else return `${(num * 0.000001).toFixed(1)}Tr`;
 };
 
